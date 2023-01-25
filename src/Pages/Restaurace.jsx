e78eaad9ed682b45fc6efab5508cc056b6690ffa
Clonethis <jirka.brtnik@post.cz>
2023-01-25 17:31:33 +0100

    Change text in Restauce/en

--- src/Pages/Restaurace.jsx
+++ src/Pages/Restaurace.jsx
@@ -1,61 +1,83 @@
 // import Header from "../elements/Header";
 import OnlyLogo from "../elements/OnlyLogo";
 // import Image from "../elements/Image";
-import photo from "../resources/images/content/food5.jpg"
-import photo2 from "../resources/images/content/food6.jpg"
+import photo from "../resources/images/content/food5.jpg";
+import photo2 from "../resources/images/content/food6.jpg";
 
-import TwoBox from "../elements/TwoBox";
 import Images from "../elements/Images";
+import TwoBox from "../elements/TwoBox";
 
 export default function Restaurace() {
-    return (
-        <div className="page">
-            <OnlyLogo />
-            <div className="content">
-                <div className="text">
-                    <h1 className="cssanimation fadeInBottom">Restaurace</h1>
-                    <div className="paragraph">
+  return (
+    <div className="page">
+      <OnlyLogo />
+      <div className="content">
+        <div className="text">
+          <h1 className="cssanimation fadeInBottom">Restaurace</h1>
+          <div className="paragraph">
+            V příjemné atmosféře restaurace hotelu Vista pro Vás připravujeme
+            snídaně, obědy, večeře či zajistíme catering.
+            {/* <a href="https://www.vista-hotel.cz/files/2021/10/962d7f3df4ab86fef915cd9c82d33535.pdf">Vista Restaurace jídelní lístek</a> */}
+          </div>
 
-                        V příjemné atmosféře restaurace hotelu Vista pro Vás připravujeme snídaně, obědy, večeře či zajistíme catering.
-                        {/* <a href="https://www.vista-hotel.cz/files/2021/10/962d7f3df4ab86fef915cd9c82d33535.pdf">Vista Restaurace jídelní lístek</a> */}
-                    </div>
-
-                    <div className="paragraph">
-                        {/* Telefon a Internet
+          <div className="paragraph">
+            {/* Telefon a Internet
 Při volání z pokoje do pokoje, vytočte číslo požadovaného pokoje a potvrďte tlačítkem SEND na telefonu.
 Nabízíme Vám bezplatné bezdrátové internetové připojení (Wi-Fi) na všech pokojích a ostatních veřejných prostor hotelu.  K připojení není potřeba žádné heslo.  
  */}
-                        <div className="paragraph">Snídaně podáváme pro ubytované, ale i pro neubytované hosty v prostorách restaurace v čase od <div className="strong"> 7:00 do 10:00</div>.	</div>
-                        <div className="paragraph">V čase od <div className="strong">11:00 do 14:00</div> v restauraci nabízíme polední menu, složené z polévky a nabídky několika hlavních jídel. Denní nabídku naleznete na našich www stránkách v sekci restaurace – týdenní menu.</div>
-                        <div className="paragraph">Restaurace je pro naše hosty otevřena denně <div className="strong">od 11:00 do 22:00</div>. V průběhu celého dne si můžete vybrat pokrmy z našeho jídelního lístku. </div>
-                        <div className="paragraph">Případně můžete využít službu Room service. Objednat si můžete pomocí telefonu v pokoji. Stačí zavolat na č. 94 a objednat si u kolegů v restauraci v čase od <div className="strong">12:00 do 21:30</div>. Jídelní lístek naleznete zde: <a href="https://www.vista-hotel.cz/cs/restaurace">Restaurace - Hotel Vista Brno</a></div>
-                    </div>
-                    <div className="paragraph">
-
-                        V hotelu jsme nezapomněli ani na naše nejmenší. Součástí přilehlých prostor hotelové restaurace je  dětský koutek, ve kterém se děti zabaví po čas celého Vašeho pobytu.
-                    </div>
-
-
-
-
-                    <h2>
-
-                        Cateringové služby
-                    </h2>
-                    <div className="paragraph">
-
-                        Nabízíme Vám využití našich cateringových služeb do Vámi vybraných prostor v Brně a v blízkém okolí. Nabídku sortimentu dle Vašich požadavků Vám zpracuje naše obchodní oddělení.
-                    </div>
-                    <div className="paragraph">
-                        Kontaktní email:  <a href="mailto:akce@vista-hotel.cz
-">akce@vista-hotel.cz
-                        </a></div>
-                </div>
-
-
+            <div className="paragraph">
+              Snídaně podáváme pro ubytované, ale i pro neubytované hosty v
+              prostorách restaurace ve všední dny od{" "}
+              <div className="strong"> 7:00 do 10:00</div>, o víkendu od{" "}
+              <div className="strong"> 7:00 do 10:30</div>.
             </div>
-            <TwoBox />
-            <Images name={photo} name2={photo2} />
+            <div className="paragraph">
+              V čase od <div className="strong">11:00 do 14:00</div>{" "}
+              v restauraci nabízíme polední menu, složené z polévky a nabídky
+              několika hlavních jídel. Denní nabídku naleznete na našich www
+              stránkách v sekci restaurace – týdenní menu.
+            </div>
+            <div className="paragraph">
+              Restaurace je pro naše hosty otevřena denně{" "}
+              <div className="strong">od 11:00 do 22:00</div>. V průběhu celého
+              dne si můžete vybrat pokrmy z našeho jídelního lístku.{" "}
+            </div>
+            <div className="paragraph">
+              Případně můžete využít službu Room service. Objednat si můžete
+              pomocí telefonu v pokoji. Stačí zavolat na č. 94 a objednat si u
+              kolegů v restauraci v čase od{" "}
+              <div className="strong">12:00 do 21:30</div>. Jídelní lístek
+              naleznete zde:{" "}
+              <a href="https://www.vista-hotel.cz/cs/restaurace">
+                Restaurace - Hotel Vista Brno
+              </a>
+            </div>
+          </div>
+          <div className="paragraph">
+            V hotelu jsme nezapomněli ani na naše nejmenší. Součástí přilehlých
+            prostor hotelové restaurace je dětský koutek, ve kterém se děti
+            zabaví po čas celého Vašeho pobytu.
+          </div>
+
+          <h2>Cateringové služby</h2>
+          <div className="paragraph">
+            Nabízíme Vám využití našich cateringových služeb do Vámi vybraných
+            prostor v Brně a v blízkém okolí. Nabídku sortimentu dle Vašich
+            požadavků Vám zpracuje naše obchodní oddělení.
+          </div>
+          <div className="paragraph">
+            Kontaktní email:{" "}
+            <a
+              href="mailto:akce@vista-hotel.cz
+"
+            >
+              akce@vista-hotel.cz
+            </a>
+          </div>
         </div>
-    );
+      </div>
+      <TwoBox />
+      <Images name={photo} name2={photo2} />
+    </div>
+  );
 };
--- src/Pages/en/Restauraceen.jsx
+++ src/Pages/en/Restauraceen.jsx
@@ -1,53 +1,67 @@
 
-import OnlyLogoen from "../../elements/OnlyLogoen";
 import Images from "../../elements/Images";
+import OnlyLogoen from "../../elements/OnlyLogoen";
+import TwoBoxen from "../../elements/TwoBoxen";
 import photo from "../../resources/images/content/food4.jpg";
 import photo2 from "../../resources/images/content/food5.jpg";
-import TwoBoxen from "../../elements/TwoBoxen";
 export default function Restauraceen(props) {
   return (
     <div className="page">
       <OnlyLogoen />
       <div className="content">
         <div className="text">
-          <h1 className="cssanimation fadeInBottom">
-            Restaurant
-          </h1>
+          <h1 className="cssanimation fadeInBottom">Restaurant</h1>
           <div className="paragraph">
-
-            In the pleasant atmosphere of the Vista hotel restaurant, we prepare breakfast, lunch, dinner or catering for conference events.
-
+            In the pleasant atmosphere of the Vista hotel restaurant, we prepare
+            breakfast, lunch, dinner or catering for conference events.
           </div>
 
-
-
-          <div className="paragraph">We serve breakfast for guests, but also for non-accommodated guests in the restaurant from <div className="strong">7am to 10am.</div></div>
-          <div className="paragraph">We offer a lunch menu from <div className="strong">11am till 2pm</div> in the restaurant, which consists of soup and several main courses. You can find the daily menu on our website in the restaurant section - weekly menu.</div>
-          <div className="paragraph">The restaurant is open daily for our guests <div className="strong">from 11am till 10pm</div>. Throughout the day you can choose dishes from our menu.</div>
-          <div className="paragraph">Alternatively, you can use the Room service. You can order by telephone in your room. Just call No. 94 and place your order with staff in the restaurant from <div className="strong">12pm to 10:30pm.</div></div>
-          <div className="paragraph">You will find the menu at the bottom of this document: <a href="https://www.vista-hotel.cz/files/2021/10/962d7f3df4ab86fef915cd9c82d33535.pdf">Vista Restaurant menu</a></div>
-
           <div className="paragraph">
-
-            We did not forget about our little ones in the hotel. Part of the common area of the hotel restaurant is a children's play zone, where children can have fun during your stay.
-          </div>
-
-
-
-
-          <h2>
-
-            Catering services
-          </h2>
-          <div className="paragraph">
-
-            We offer our catering services in your chosen premises in Brno and the surrounding area. Our sales department will prepare an assortment offer according to your requirements.
+            We serve breakfast for guests, but also for non-accommodated guests
+            in the restaurant from
+            <div className="strong"> 7:00 do 10:00</div>, and at the weekend at{" "}
+            <div className="strong"> 7:00 do 10:30</div>.
           </div>
           <div className="paragraph">
-
-            Contact email:  <a href="mailto:akce@vista-hotel.cz">akce@vista-hotel.cz</a>
+            We offer a lunch menu from{" "}
+            <div className="strong">11am till 2pm</div> in the restaurant, which
+            consists of soup and several main courses. You can find the daily
+            menu on our website in the restaurant section - weekly menu.
+          </div>
+          <div className="paragraph">
+            The restaurant is open daily for our guests{" "}
+            <div className="strong">from 11am till 10pm</div>. Throughout the
+            day you can choose dishes from our menu.
+          </div>
+          <div className="paragraph">
+            Alternatively, you can use the Room service. You can order by
+            telephone in your room. Just call No. 94 and place your order with
+            staff in the restaurant from{" "}
+            <div className="strong">12pm to 10:30pm.</div>
+          </div>
+          <div className="paragraph">
+            You will find the menu at the bottom of this document:{" "}
+            <a href="https://www.vista-hotel.cz/files/2021/10/962d7f3df4ab86fef915cd9c82d33535.pdf">
+              Vista Restaurant menu
+            </a>
           </div>
 
+          <div className="paragraph">
+            We did not forget about our little ones in the hotel. Part of the
+            common area of the hotel restaurant is a children's play zone, where
+            children can have fun during your stay.
+          </div>
+
+          <h2>Catering services</h2>
+          <div className="paragraph">
+            We offer our catering services in your chosen premises in Brno and
+            the surrounding area. Our sales department will prepare an
+            assortment offer according to your requirements.
+          </div>
+          <div className="paragraph">
+            Contact email:{" "}
+            <a href="mailto:akce@vista-hotel.cz">akce@vista-hotel.cz</a>
+          </div>
         </div>
       </div>
       <TwoBoxen />
